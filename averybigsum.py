5d9ec3fec4c734c273f790666b9747f26869ea9b
Lê Ngọc Minh Phương <minhphuong6417@gmail.com>
2026-01-05 23:34:42 +0700

    Add files via upload

---
+++ averybigsum.py
@@ -0,0 +1,19 @@
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+    result = 0
+
+    for i in ar:
+        result += i
+    
+    return result
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    ar = list(map(int, input().rstrip().split()))
+    result = aVeryBigSum(ar)
+    print(result)
